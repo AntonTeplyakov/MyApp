58a971744371562b69656682cf28f913f15e73bc
AntonTeplyakov <jsxscript@gmail.com>
2022-01-26 18:26:44 +1000

    play sandbox

---
+++ myapp/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [name, setName] = useState('mario')
+  const [events, setEvents] = useState([
+    {title: "marios's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
+  // let name = 'mario'
+  const handleClick = (id) =>{
+    console.log(id)
+  }
+  return (
+    <div className="App">
+      {events.map((event,index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={()=> handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
